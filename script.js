ba2a2a5677ff6b50e7f4863539073f18fac1e552
PrimeSK <kstgalle@gmail.com>
2024-02-23 04:05:45 +0530

    Changed scroll button color

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ let calcScrollValue = () => {
 	scrollProgress.addEventListener("click", () => {
 	  document.documentElement.scrollTop = 0;
 	});
-	scrollProgress.style.background = `conic-gradient(#2E3594 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+	scrollProgress.style.background = `conic-gradient(#77C1E0 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
   };
   
   window.onscroll = calcScrollValue;
